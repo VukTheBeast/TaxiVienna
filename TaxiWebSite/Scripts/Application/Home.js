327c1e86a5a871f500170785b408e4d42a6a935e
dragomix2001 <dragomix2001@gmail.com>
2014-11-26 23:10:45 +0100

    doradio pozadinu temperaturu, i jos par sitnica pokusao

---
+++ TaxiWebSite/Scripts/Application/Home.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    var icon;
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?q=Vienna,at",
+            dataType: "json"
+        }).done(function (data) {
+            var a, b;
+            icon = data.weather[0].icon;            
+            a = parseInt(data.main["temp"]);
+            b = a - 273;
+            $("#temperatura").html(b + "°C");
+            $("#ikonica").attr('src', "http://openweathermap.org/img/w/" + icon + ".png");
+        });        
+});
