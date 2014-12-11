b5c401163eae48d116480a364810dfebd86c5313
Vuk Manic <vukmanic@outlook.com>
2014-12-11 23:13:39 +0100

    Izvukao podatke iz baze, treba doraditi!!...
    doraditi sutra na poslu

--- TaxiWebSite/Controllers/AdminPanelController.cs
+++ TaxiWebSite/Controllers/AdminPanelController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using TaxiWebSite.Models;
 
 namespace TaxiWebSite.Controllers
 {
@@ -16,7 +18,21 @@ namespace TaxiWebSite.Controllers
 
         [HttpPost]
         public JsonResult DAL() {
+            
+            using (var dbContext = new DB_9B8AB0_taxiEntities()) {
+                var ukRezervacija = dbContext.Rezervacije.ToList();
+                //var statistika = dbContext.Rezervacije.SqlQuery("select MONTH(DatumVreme), Count(DatumVreme) from Rezervacije group by Month(DatumVreme)");
 
+                var r = (from statist in ukRezervacija
+                         group statist by statist.DatumVreme.Month into statiGroup
+                         orderby statiGroup.Key
+                         select statiGroup).ToList();
+                int jan = r.ElementAt(0).Count();
+                //var result = dbContext.Rezervacije.GroupBy(m => m.DatumVreme.Month).ToList();
+                //var result = from rez in ukRezervacija
+                //             group rez by rez.DatumVreme.Month into Mesec
+                //             select Mesec;
+            }
             string Jan = "4";
             string Feb = "5";
             string Mar = "7";
